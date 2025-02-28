13c3bd35602ddaebe973f6fcc8a0ce78cb9e74b0
fiivxyxxng <scx200800@gmail.com>
2025-02-28 14:45:03 +0900

    refactor: baseURL articles path 분리

--- api/ArticleService.js
+++ api/ArticleService.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'https://sprint-mission-api.vercel.app/articles'
-})
+  baseURL: "https://sprint-mission-api.vercel.app",
+});
 
 /**
  * 게시글 목록 조회
@@ -10,85 +10,90 @@ const instance = axios.create({
  * @param {number} pageSize
  * @param {string} [keyword]
  */
-export const getArticleList = async params => {
-  return await instance.get('/', { params })
-    .then(response => {
+export const getArticleList = async (params) => {
+  return await instance
+    .get("/articles", { params })
+    .then((response) => {
       return response.data;
     })
-    .catch(error => {
+    .catch((error) => {
       if (error.response) {
         console.log(error.response.status);
       } else {
         console.log(error.message);
       }
-    })
-}
+    });
+};
 
 /**
  * 게시글 상세 조회
  */
-export const getArticle = async id => {
-  return await instance.get(`/${id}`)
-  .then(response => {
-    return response.data;
-  })
-  .catch(error => {
-    if (error.response) {
-      console.log(error.response.status, '게시글을 찾을 수 없음');
-    } else {
-      console.log(error.message);
-    }
-  })
-}
+export const getArticle = async (id) => {
+  return await instance
+    .get(`/articles/${id}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      if (error.response) {
+        console.log(error.response.status, "게시글을 찾을 수 없음");
+      } else {
+        console.log(error.message);
+      }
+    });
+};
 
 /**
  * 게시글 등록
  */
-export const createArticle = async data => {
-  return await instance.post('/', data)
-  .then(response => {
-    return response.data;
-  })
-  .catch(error => {
-    if (error.response) {
-      console.log(error.response.status, '유효성 검사 오류');
-    } else {
-      console.log(error.message);
-    }
-  })
-}
+export const createArticle = async (data) => {
+  return await instance
+    .post("/articles", data)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      if (error.response) {
+        console.log(error.response.status, "유효성 검사 오류");
+      } else {
+        console.log(error.message);
+      }
+    });
+};
 
 /**
  * 게시글 수정
  */
 export const patchArticle = async (id, data) => {
-  return await instance.patch(`/${id}`, data)
-  .then(response => {
-    return response.data;
-  })
-  .catch(error => {
-    if (error.response) {
-      console.log(error.response.status, '게시글을 찾을 수 없음');
-    } else {
-      console.log(error.message);
-    }
-  })
-}
+  return await instance
+    .patch(`/articles/${id}`, data)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      if (error.response) {
+        console.log(error.response.status, "게시글을 찾을 수 없음");
+      } else {
+        console.log(error.message);
+      }
+    });
+};
 
 /**
  * 게시글 삭제
  */
-export const deleteArticle = async id => {
-  return await instance.delete(`/${id}`)
-  .then(response => {
-    console.log(response.status, '성공적으로 삭제됨');
-    return response.data;
-  })
-  .catch(error => {
-    if (error.response) {
-      console.log(error.response.status, '게시글을 찾을 수 없음');
-    } else {
-      console.log(error.message);
-    }
-  })
-}
+export const deleteArticle = async (id) => {
+  return await instance
+    .delete(`/articles/${id}`)
+    .then((response) => {
+      console.log(response.status, "성공적으로 삭제됨");
+      return response.data;
+    })
+    .catch((error) => {
+      if (error.response) {
+        console.log(error.response.status, "게시글을 찾을 수 없음");
+      } else {
+        console.log(error.message);
+      }
+    });
+};
